d3a28c32ea6976959b19164e2e1a68bdbdce45e9
Alumno <alumno@admins-macbook-pro.local>
2017-03-09 21:59:19 -0500

    first commit

---
+++ HomeWork05/Contact.swift
@@ -0,0 +1,17 @@
+//
+//  Contact.swift
+//  HomeWork05
+//
+//  Created by Alumno on 9/03/17.
+//  Copyright © 2017 JorgeLimo. All rights reserved.
+//
+
+import UIKit
+
+class Contact: NSObject {
+
+    var name:String!
+    var number:String!
+    var age:Int!
+    
+}
